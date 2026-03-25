93ef74f9a02ffe1724667abad30b90ec1f7d3a9e
pss121106 <pss1@kent.ac.uk>
2026-03-25 18:44:16 +0000

    file_I/O_ feature

--- blackjack_game.py
+++ blackjack_game.py
@@ -11,4 +11,15 @@ def validate_user():
         if pattern.fullmatch(name):
             return name
         print("Invalid name. Letters or Numbers only!")
+
+def file_operations(name=None, balance=None, mode='read'):
+    if mode == 'write':
+        with open(SAVE_FILE, "w") as f:
+            f.write(f"{name},{balance}")
+    else:
+        if os.path.exists(SAVE_FILE):
+            with open(SAVE_FILE,"r") as f:
+                data = f.read().split(,)
+                return data[0], int(data[1])
+        return None, 1000
         
