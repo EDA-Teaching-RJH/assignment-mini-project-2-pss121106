c12e0bdac5de8fe216dcfc673896a943af87859f
pss121106 <pss1@kent.ac.uk>
2026-03-25 18:50:28 +0000

    parallel_list_hand_values

--- blackjack_game.py
+++ blackjack_game.py
@@ -19,7 +19,15 @@ def file_operations(name=None, balance=None, mode='read'):
     else:
         if os.path.exists(SAVE_FILE):
             with open(SAVE_FILE,"r") as f:
-                data = f.read().split(,)
+                data = f.read().split(",")
                 return data[0], int(data[1])
         return None, 1000
-        
+
+def get_hand_score(hand):
+    values = [c.value() for c in hand]
+    is_ace = [c.rank == 'A' for c in hand]
+    total = sum(values)
+    for i in range(len(values)):
+        if total > 21 and is_ace[i]:
+            total-= 10
+    return total 
