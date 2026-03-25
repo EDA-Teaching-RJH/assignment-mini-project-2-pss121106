d8c639c41db5184590d8682b1c0d9969cf4dd5ee
pss121106 <pss1@kent.ac.uk>
2026-03-25 15:54:14 +0000

    symbolsuits

--- deckofcards.py
+++ deckofcards.py
@@ -6,3 +6,12 @@ class card:
         'Hearts': '♥',
         'Diamonds': '♦'
     }
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank 
+        self.symbol = self.suit_codes.get(suit, suit)
+    def value(self)
+        if self.rank == 'A': return 11
+        return int(self.rank)
+    def __str__(self):
+        return f"[{self.rank}{self.symbol}"
