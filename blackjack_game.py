64d48dd780bb42240ff94c95a55db247ffb39258
pss121106 <pss1@kent.ac.uk>
2026-03-25 18:35:19 +0000

    regexnamecheck

--- blackjack_game.py
+++ blackjack_game.py
@@ -3,3 +3,12 @@ import os
 import deckofcards
 
 SAVE_FILE = "save.txt"
+
+def validate_user():
+    pattern = re.compile(r"^[a-zA-Z0-9]{3,10}$")
+    while True: 
+        name = input("Enter name")
+        if pattern.fullmatch(name):
+            return name
+        print("Invalid name. Letters or Numbers only!")
+        
